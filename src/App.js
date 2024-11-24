e30f81cf2763729cf6cfc739c2a0ca7eaa4bd734
nayanatara07 <nayana2017tara@gmail.com>
2024-11-24 13:05:29 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const url = "http://localhost:8080/";
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((d) => setData(d))
+      .catch((err) => console.log(err));
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Online Store</h1>
+      <hr></hr>
+      <div style={{display:'flex',justifyContent:'center'}}>
+        {data.map((value, index) => (
+          <div style={{backgroundColor:'orange',margin:'5px',padding:'10px'}} key={index}>
+            <h3>{value.name}</h3>
+            <h4>{value.price}</h4>
+            <button>Add to Cart</button>
+            </div>
+        ))}
+      </div>
     </div>
   );
 }
